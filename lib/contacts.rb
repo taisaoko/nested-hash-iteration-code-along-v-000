b9df25af44bcde687933bbbd4c7c4d71f422c74c
taisaoko <ttd3calvin@gmail.com>
2018-08-26 19:24:08 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,5 +17,5 @@ require 'pry'
 def remove_strawberry(contacts)
     contacts["Freddy Mercury"].each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
-        
+        value.
 end
